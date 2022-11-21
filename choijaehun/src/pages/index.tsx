6bce445b014c85efa856461d7232b1b523b4db28
zoeyourlife <201814112@office.skhu.ac.kr>
2022-11-22 01:48:12 +0900

    chore: 1차 push

--- choijaehun/src/pages/index.tsx
+++ choijaehun/src/pages/index.tsx
@@ -1,3 +1,12 @@
+import styled from "styled-components";
+import Layout from "../components/Layout";
+import GlobalStyle from "./GlobalStyle";
+
 export default function Home() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <Layout name="Piki" content="I'm happy today" />
+    </>
+  );
 }
