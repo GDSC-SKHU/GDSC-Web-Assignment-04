9ec7a1a4ab9664027cf147ff6089922d790a228e
NohWookJin <jinn98@kakao.com>
2022-11-22 16:42:07 +0900

    feat: Header, Main Component 추가

--- nohwookjin/src/pages/index.tsx
+++ nohwookjin/src/pages/index.tsx
@@ -1,7 +1,18 @@
+import styled from "styled-components";
+
+import Header from "../components/Header";
+import Main from "../components/Main";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Blog</h1>
-    </div>
+    <StyledDiv>
+      <Header />
+      <Main />
+    </StyledDiv>
   );
 }
+
+const StyledDiv = styled.div`
+  width: 100vw;
+  height: 150vh;
+`;
