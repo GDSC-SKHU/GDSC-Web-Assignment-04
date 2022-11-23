67db39c8d2d90bed1c42a5b489d3cf0e0950bb7f
khj0426 <706shin1728@naver.com>
2022-11-23 19:05:18 +0900

    feat:Board랜더링

--- kimhyojung/pages/index.tsx
+++ kimhyojung/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Board from './components/Board';
 
 export default function Home() {
-  return <div></div>;
+  return <Board />;
 }
