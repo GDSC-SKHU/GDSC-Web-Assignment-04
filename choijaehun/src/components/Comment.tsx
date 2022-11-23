3a1e4a149f4d6c60f92074d57a3e269b73d28b44
zoeyourlife <201814112@office.skhu.ac.kr>
2022-11-23 13:58:53 +0900

    feat: 마지막 댓글 이름 적기 기능

--- choijaehun/src/components/Comment.tsx
+++ choijaehun/src/components/Comment.tsx
@@ -5,21 +5,26 @@ import ProfileImage from "./ProfileImage";
 
 interface IComment {
   id: number;
-  // name: string;
+  name: string;
   content: string;
 }
 
 const INPUT_ID = "commeninput";
 
 function Comment() {
-  const [comments, setComments] = useState<IComment[]>([
-  ]);
+  const [comments, setComments] = useState<IComment[]>([]);
 
   const [currentComment, setCurrentComment] = useState<string>("");
+  const [currentName, setCurrentName] = useState<string>("");
 
   const onCurrentCommentChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setCurrentComment(value);
+  };
+
+  const onCurrentNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setCurrentName(value);
   };
 
   const onCurrentSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -27,7 +32,7 @@ function Comment() {
 
     setComments((prev) => [
       ...prev,
-      { id: prev.length + 1, content: currentComment },
+      { id: prev.length + 1, name: currentName, content: currentComment },
     ]);
 
     setCurrentComment("");
@@ -39,13 +44,23 @@ function Comment() {
       <StyledHr />
       {comments.map((comment) => (
         <div key={comment.id}>
-          <h2></h2>
-          <StyledCommet>{comment.content}</StyledCommet>
+          <StyledCommet>
+          <h2>{comment.name}</h2>
+          <h2>{comment.content}</h2>
+          </StyledCommet>
         </div>
       ))}
 
       <StyledForm onSubmit={onCurrentSubmit}>
-        <label htmlFor={INPUT_ID}><CommentProfile /></label>
+        <label htmlFor={INPUT_ID}>
+          <CommentProfile />
+        </label>
+        <StyledNameInput
+          id={INPUT_ID}
+          type="text"
+          value={currentName}
+          onChange={onCurrentNameChange}
+        />
         <StyledInput
           id={INPUT_ID}
           type="text"
@@ -61,48 +76,58 @@ function Comment() {
 export default Comment;
 
 const StyledHr = styled.hr`
-margin-top: 10px;
-`
+  margin-top: 10px;
+`;
 
 const StyledH3 = styled.h3`
-margin-top: 10px;
-`
+  margin-top: 10px;
+`;
 
 const StyledForm = styled.form`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-top: 20px;
-`
+`;
+
+const StyledNameInput = styled.input`
+  width: 50px;
+  padding: 8px;
+  border-radius: 15px;
+  border: 1px solid #e4e4e4;
+  background: #ffffff;
+`;
+
 const StyledInput = styled.input`
-  width:280px;
+  width: 220px;
   padding: 8px;
   border-radius: 15px;
   border: none;
   background: #e4e4e4;
   :hover,
-  :active{
+  :active {
     background: #f5f5f5;
     border: 1px solid #ccc;
   }
-  :focus{
+  :focus {
     background: #f5f5f5;
     outline: none;
   }
-`
+`;
 const StyledButton = styled.button`
   padding: 10px;
   border: 1px solid #ccc;
   background-color: #f5f5f5;
   border-radius: 25px;
-  :hover{
+  :hover {
     background-color: #c5c0c0;
     color: white;
-    transition: all .3s;
-    cursor:pointer;
+    transition: all 0.3s;
+    cursor: pointer;
   }
-`
+`;
 const StyledCommet = styled.h2`
-  font-size: 18px;
-  text-align: center;
-`
+  display: flex;
+  font-size: 10px;
+  justify-content: space-between;
+`;
