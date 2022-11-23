2d8a16624e1b151682465620af3b0ef3714c7fee
NohWookJin <jinn98@kakao.com>
2022-11-23 12:38:24 +0900

    style: 컬러 일부 수정

--- nohwookjin/styles/global-style.ts
+++ nohwookjin/styles/global-style.ts
@@ -5,8 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        color: #ececec;
+        color:#ececec;
         background-color: #121212;
-        
     }
 `;
