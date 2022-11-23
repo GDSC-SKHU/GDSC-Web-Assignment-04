971c874589c90a894923b6432414cf58c8cbb80d
sinamong0620 <sinaskim60@gmail.com>
2022-11-23 10:13:54 +0900

    feat:onCurrentCommentChange function add currentComment & comments variation add

--- kimshinah/introduce_comment_project/src/components/RightSidebar.tsx
+++ kimshinah/introduce_comment_project/src/components/RightSidebar.tsx
@@ -1,19 +1,41 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import styled from "styled-components";
 
-const rightSidebar = () => {
+interface IComment {
+  id: number;
+  content: string;
+}
+
+const RightSidebar = () => {
+  const [comments, setComments] = useState<IComment[]>([
+    { id: 0, content: "hello" },
+  ]);
+  const [currentComment, setcurrentComment] = useState<string>("");
+
+  const onCurrentTodoChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target; //{value}랑 ChangeEvent<HTMLInputElement>가 뭔지 모르겠네..
+    setcurrentComment(value);
+    console.log(currentComment);
+  };
   return (
     <RightSidebarCss>
       <header>꺄앍</header>
+
       <section>
         <form>
-          <input />
+          <input
+            id="commentInput"
+            type="text"
+            value={currentComment}
+            onChange={onCurrentTodoChange}
+          />
           <button>작성</button>
         </form>
       </section>
     </RightSidebarCss>
   );
 };
-export default rightSidebar;
+export default RightSidebar;
 
 const RightSidebarCss = styled.div`
   display: flex;
