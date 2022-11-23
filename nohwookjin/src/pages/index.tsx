ef663ac7b853bab95cfc82b81392e78f6c6095ec
NohWookJin <jinn98@kakao.com>
2022-11-23 12:37:55 +0900

    feat: SideBar Component 추가

--- nohwookjin/src/pages/index.tsx
+++ nohwookjin/src/pages/index.tsx
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Sidebar from "../components/SideBar";
 
 export default function Home() {
   return (
     <StyledDiv>
       <Header />
+      <Sidebar />
       <Main />
     </StyledDiv>
   );
